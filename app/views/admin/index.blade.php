d930176cdb60c4b63ed8bc32c545576850a8f7d6
loydclifford <loydfordfarrow@yahoo.com>
2015-02-06 12:41:37 -0800

    UPDATE UI

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -1,81 +1,73 @@
 @extends('admin._partials._layout')
 
 @section('main-content')
-<div class="col-lg-6">
+<div class="row"><p>Filter Status</p>
+   <div class="col-lg-6">
     <div class="input-group">
-      <input type="text" class="form-control" aria-label="...">
-      <div class="input-group-btn">
-        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action <span class="caret"></span></button>
+      <input style="position: relative; width: 150px; left: 200px;" type="text" class="form-control" aria-label="...">
+      <div class="input-group-btn meo_year">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">S/Y<span class="caret"></span></button>
         <ul class="dropdown-menu dropdown-menu-right" role="menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
+          <li><a href="#">2009-10</a></li>
+          <li><a href="#">2010-11</a></li>
+          <li><a href="#">2011-12</a></li>
+          <li><a href="#">2012-13</a></li>
         </ul>
       </div><!-- /btn-group -->
     </div><!-- /input-group -->
   </div><!-- /.col-lg-6 -->
- 
-<!-- Main row -->
-<div class="row">
-    <!-- Left col -->
-    <section class="col-lg-12 connectedSortable meo_body">
+
+  <div class="col-lg-6">
+    <div class="input-group">
+      <input type="text" class="form-control" aria-label="...">
+      <div class="input-group-btn meo_semester">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Sem<span class="caret"></span></button>
+        <ul class="dropdown-menu dropdown-menu-right" role="menu">
+          <li><a href="#">1st Sem</a></li>
+          <li><a href="#">2nd Sem</a></li>
+          
+        </ul>
+      </div><!-- /btn-group -->
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+</div><!-- /.last-row -->
+
+<div class="row"><!-- Main row -->
+   
         <!-- Custom tabs (Charts with tabs)-->
         <div class="nav-tabs-custom romeo_head">
-            <!-- Tabs within a box -->
-            <ul class="nav nav-tabs pull-right">
-                <li class="pull-left header"><i class="fa fa-inbox"></i> My Account </li>
-            </ul>
-            <div class="tab-content no-padding">
+            <div class="tab-content">
                 <!-- big buttons holder-->
-                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; display: inline;">
-                    <ul class="nav" style="position: relative; border: solid 2px #696969; height: 100px; width: 150px; padding: 10px;">
+                <div class="chart tab-pane meo_body" id="revenue-chart" style="position: relative; display: inline;">
+                    
+                    <ul class="nav meo_courses" >
                         <i class="fa fa-book fa-3x" style="position: relative; border: solid 1px transparent;"></i>
                         <li class="header" style="position: relative; float: right; top: 12px;"> Total Courses </li>
                     </ul>
-                </div>
-            </div>
-             
-            <div class="tab-content" style="position: absolute; top: 27px; left: 245px; ">
-                <!-- big buttons holder-->
-                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; display: inline;">
-                    <ul class="nav" style="position: relative; border: solid 2px #696969; height: 100px; width: 150px; padding: 10px;">
+                             
+        
+                    <ul class="nav meo_teachers">
+                        <i class="glyphicon glyphicon-user" style="position: relative; border: solid 1px transparent;"></i>
+                        <li class="header" style="position: relative; float: right; top: 12px;"> Total Teachers </li>
+                    </ul>
+              
+                    <ul class="nav meo_students">
                         <i class="fa fa-book fa-3x" style="position: relative; border: solid 1px transparent;"></i>
                         <li class="header" style="position: relative; float: right; top: -30px;"> Total Students </li>
                     </ul>
-                </div>
-            </div>
-
-            <div class="tab-content" style="position: absolute; top: 27px; left: 490px; ">
-                <!-- big buttons holder-->
-                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; display: inline;">
-                    <ul class="nav" style="position: relative; border: solid 2px #696969; height: 100px; width: 150px; padding: 10px;">
-                        <i class="fa fa-book fa-3x" style="position: relative; border: solid 1px transparent;"></i>
-                        <li class="header" style="position: relative; float: right; top: -30px;"> Total Students </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="tab-content" style="position: absolute; top: 27px; left: 730px; ">
-                <!-- big buttons holder-->
-                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; display: inline;">
-                    <ul class="nav" style="position: relative; border: solid 2px #696969; height: 100px; width: 150px; padding: 10px;">
+          
+                    <ul class="nav meo_subjects">
                         <i class="fa fa-book fa-3x" style="position: relative; border: solid 1px transparent;"></i>
                         <li class="header" style="position: relative; float: right; top: 12px;"> Total Subjects </li>
                     </ul>
+
+                
                 </div>
-            </div>
-
-            </br>
-			<h2>Quick Navigation</h2>
-			<h5><hr ></hr></h5>
-								 
+            </div>			 
         </div><!-- /.nav-tabs-custom -->
-    </section><!-- /.Left col -->
-
+    <!-- /.Left col -->
 </div><!-- /.row (main row) -->
-
+                
 @stop
 
 @section('after-footer')s
